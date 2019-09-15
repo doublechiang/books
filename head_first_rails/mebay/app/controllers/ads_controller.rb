f5a4373be23c7f73b4da3af5b4224d761d9b318f
Jiang Junyu <double.chiang@gmail.com>
2019-09-15 12:05:53 -0700

    Add authenticate and delete method.

--- head_first_rails/mebay/app/controllers/ads_controller.rb
+++ head_first_rails/mebay/app/controllers/ads_controller.rb
@@ -1,4 +1,5 @@
 class AdsController < ApplicationController
+  before_filter :check_logged_in, :only => [:edit, :update, :delete]
   def show
 #    p params
     @ad = Ad.find(params[:id])
@@ -30,8 +31,21 @@ class AdsController < ApplicationController
 
   end
 
+  def destroy
+    @ad = Ad.find(params[:id])
+    @ad.destroy
+    redirect_to "/ads/"
+
+  end
+
   private
   def allowed_params
     params.require(:ad).permit(:name, :description, :price, :seller_id, :email, :img_url)
+  end
+
+  def check_logged_in
+    authenticate_or_request_with_http_basic("Ads") do | username, password |
+      username == "admin" && password == "259706"
+    end
   end
 end
