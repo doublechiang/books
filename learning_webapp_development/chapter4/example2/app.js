d046e327a3f7c0035ddab52286b323db58a73ebb
Jiang Junyu <double.chiang@gmail.com>
2020-01-08 14:19:04 -0800

    Support fadin for mouse click and keyboard enter pressed.

--- learning_webapp_development/chapter4/example2/app.js
+++ learning_webapp_development/chapter4/example2/app.js
@@ -1,25 +1,26 @@
 var main = function () {
     "use strict";
-    $(".comment-input button").on("click", function(event) {
+
+    var addCommentFromInputBox = function() {
+        var $new_comment;
         if ($(".comment-input input").val() !== "") {
-            var $new_comment =$("<p>").text($(".comment-input input").val());
+            $new_comment =$("<p>").text($(".comment-input input").val());
+            $new_comment.hide();
             $(".comments").append($new_comment);
+            $new_comment.fadeIn();
             $(".comment-input input").val("");
         }
+
+
+    }
+    $(".comment-input button").on("click", function(event) {
+        addCommentFromInputBox();
     });
 
     $(".comment-input input").on("keypress", function(event) {
-        //console.log("key pressed" + event.keyCode);
-        var $new_comment;
         if (event.keyCode ===13) {
-            console.log("key pressed" + event.keyCode);
-            if ($(".comment-input input").val() !== "") {
-                $new_comment =$("<p>").text($(".comment-input input").val());
-                $(".comments").append($new_comment);
-                $(".comment-input input").val("");
-            }
+            addCommentFromInputBox();
         }
-
     });
 
 
