54f0f8a8d9e4b68ce2067c6e2531f1d9df1a3d3a
Jiang Junyu <double.chiang@gmail.com>
2020-01-07 17:54:20 -0800

    add support to enter key.

--- learning_webapp_development/chapter4/example2/app.js
+++ learning_webapp_development/chapter4/example2/app.js
@@ -7,6 +7,22 @@ var main = function () {
             $(".comment-input input").val("");
         }
     });
+
+    $(".comment-input input").on("keypress", function(event) {
+        //console.log("key pressed" + event.keyCode);
+        var $new_comment;
+        if (event.keyCode ===13) {
+            console.log("key pressed" + event.keyCode);
+            if ($(".comment-input input").val() !== "") {
+                $new_comment =$("<p>").text($(".comment-input input").val());
+                $(".comments").append($new_comment);
+                $(".comment-input input").val("");
+            }
+        }
+
+    });
+
+
 };
 
 $(document).ready(main);
