1c9c3376ff6477ea9ca2945acacfaa7606df3385
Jiang Junyu <double.chiang@gmail.com>
2020-02-02 16:14:58 -0800

    fixes #10, producing reuslt as HTML

--- head_first_python/ch5/vsearch4web.py
+++ head_first_python/ch5/vsearch4web.py
@@ -12,9 +12,15 @@ def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to search4letters on teh web!')
 
 @app.route('/search4', methods=['POST'])
-def do_search() -> str:
+def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
-    return str(search4letters(phrase, letters))
+    title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html',
+                            the_phrase=phrase,
+                            the_letters=letters,
+                            the_titles=title,
+                            the_results=results, )
 
 app.run(debug=True)
