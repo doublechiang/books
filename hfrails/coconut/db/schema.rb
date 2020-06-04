869abdef70fbf71d47de48f0a2bfef4e1057b82c
Jiang Junyu <double.chiang@gmail.com>
2020-06-04 12:14:13 -0700

    upgrade to use rails 6 by 'rails app:udpate' and use jquery framework

--- hfrails/coconut/db/schema.rb
+++ hfrails/coconut/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_21_195448) do
+ActiveRecord::Schema.define(version: 2020_06_04_185938) do
 
   create_table "flights", force: :cascade do |t|
     t.datetime "departure"
